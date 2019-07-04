5a55ab69317bc7c592a1a7add2d100913976e999
Tam Nguyen M <nmint8m@gmail.com>
2019-07-04 18:24:55 +0700

    Section 1 - Chapter 4 - Update example projects

--- Document/ExampleProject/04-observables-in-practice/starter/Combinestagram/MainViewController.swift
+++ Document/ExampleProject/04-observables-in-practice/starter/Combinestagram/MainViewController.swift
@@ -25,160 +25,31 @@ import RxSwift
 
 class MainViewController: UIViewController {
 
-    @IBOutlet weak var imagePreview: UIImageView!
-    @IBOutlet weak var buttonClear: UIButton!
-    @IBOutlet weak var buttonSave: UIButton!
-    @IBOutlet weak var itemAdd: UIBarButtonItem!
+  @IBOutlet weak var imagePreview: UIImageView!
+  @IBOutlet weak var buttonClear: UIButton!
+  @IBOutlet weak var buttonSave: UIButton!
+  @IBOutlet weak var itemAdd: UIBarButtonItem!
 
-    private let disposeBag = DisposeBag()
-    private let images = Variable<[UIImage]>([])
-    // # Implementing a basic uniqueness filter
-    private var imageCache = [Int]()
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  }
+  
+  @IBAction func actionClear() {
 
-        // Chapter 6: Challenge 1: Combinestagramʼs source code
+  }
 
-        let imagesObservable = images.asObservable().share()
+  @IBAction func actionSave() {
 
-        imagesObservable
-            .subscribe(onNext: { [weak self] photos in
-                guard let this = self else { return }
-                this.updateUI(photos: photos)
-            }).disposed(by: disposeBag)
+  }
 
-        // # Using throttle to reduce work on subscriptions with high load
-        imagesObservable
-            .throttle(0.5, scheduler: MainScheduler.instance)
-            .subscribe(onNext: { [unowned self] photos in
-                self.imagePreview.image = UIImage.collage(images: photos,
-                                                          size: self.imagePreview.frame.size)
-            })
-            .disposed(by: disposeBag)
-    }
+  @IBAction func actionAdd() {
 
-    private func updateUI(photos: [UIImage]) {
-        buttonSave.isEnabled = photos.count > 0 && photos.count % 2 == 0
-        buttonClear.isEnabled = photos.count > 0
-        itemAdd.isEnabled = photos.count < 6
-        title = photos.count > 0 ? "\(photos.count) photos" : "Collage"
-    }
+  }
 
-    @IBAction func actionClear() {
-        images.value = []
-        imageCache = []
-
-        // Chapter 6: Challenge 1: Combinestagramʼs source code
-        navigationItem.leftBarButtonItem = UIBarButtonItem(image: nil, style: .done,
-                                                           target: nil, action: nil)
-    }
-
-    @IBAction func actionSave() {
-        guard let image = imagePreview.image else { return }
-        /* Original version
-        PhotoWriter.save(image)
-            .asSingle()
-            .subscribe(onSuccess: { [weak self] id in
-                self?.showMessage("Saved with id: \(id)")
-                self?.actionClear()
-                }, onError: { [weak self] error in
-                    self?.showMessage("Error", description: error.localizedDescription)
-            })
-            .disposed(by: disposeBag)
-         */
-
-        // Challenge 2: Add custom observable to present alerts
-        PhotoWriter
-            .save(image)
-            .subscribe(
-                onSuccess: { [weak self] id in
-                    self?.showMessage("Saved with id: \(id)")
-                    self?.actionClear()
-                }, onError: { [weak self] error in
-                    self?.showMessage("Error", description: error.localizedDescription)
-            })
-            .disposed(by: disposeBag)
-    }
-
-    @IBAction func actionAdd() {
-        guard let viewController = storyboard!.instantiateViewController(withIdentifier: "PhotosViewController") as? PhotosViewController else { return }
-        /* Original code
-        viewController.selectedPhotos
-            .subscribe(
-                onNext: { [weak self] newImage in
-                    guard let this = self else { return }
-                    this.images.value.append(newImage)
-                },
-                onDisposed: {
-                    print("Completed photo selection")
-            }).disposed(by: disposeBag)
-        navigationController?.pushViewController(viewController, animated: true)
-         */
-
-        // # Sharing subscriptions
-        let shareSelectedPhotos = viewController.selectedPhotos.share()
-        shareSelectedPhotos
-            // # Keep taking elements while a condition is met
-            .takeWhile{ [weak self] _ in
-                guard let this = self else { return false }
-                return this.images.value.count < 6
-            }
-            // # Filtering elements you donʼt need
-            .filter { newImage in
-                return newImage.size.width > newImage.size.height
-            }
-            // # Implementing a basic uniqueness filter
-            .filter { [weak self] newImage in
-                guard let this = self else { return false }
-                let imageLenght = UIImagePNGRepresentation(newImage)?.count ?? 0
-                guard !this.imageCache.contains(imageLenght) else {
-                    return false
-                }
-                this.imageCache.append(imageLenght)
-                return true
-            }
-            .subscribe(
-            onNext: { [weak self] newImage in
-                guard let this = self else { return }
-                this.images.value.append(newImage)
-            },
-            onDisposed: {
-                print("Completed photo selection")
-            }).disposed(by: disposeBag)
-
-        shareSelectedPhotos
-            // # Ignoring all elements
-            .ignoreElements()
-            .subscribe(onCompleted: { [weak self] in
-                guard let this = self else { return }
-                this.updateNavigationIcon()
-            }).disposed(by: viewController.bag)
-
-        navigationController?.pushViewController(viewController, animated: true)
-    }
-
-    func showMessage(_ title: String, description: String = "") {
-        /* Original code
-        let alert = UIAlertController(title: title, message: description, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Close", style: .default, handler: { [weak self] _ in self?.dismiss(animated: true, completion: nil)}))
-        present(alert, animated: true, completion: nil)
-         */
-        // # Chapter 4: Challenge 2: Add custom observable to present alerts
-        alert(title: title, message: description)
-            .subscribe(onCompleted: { [weak self] in
-                guard let this = self else { return }
-                this.dismiss(animated: true, completion: nil)
-            }).disposed(by: disposeBag)
-    }
-
-    // # Ignoring all elements
-    private func updateNavigationIcon() {
-        let icon = imagePreview.image?
-            .scaled(CGSize(width: 22, height: 22))
-            .withRenderingMode(.alwaysOriginal)
-        navigationItem.leftBarButtonItem = UIBarButtonItem(image: icon, style: .done,
-                                                           target: nil, action: nil)
-    }
+  func showMessage(_ title: String, description: String? = nil) {
+    let alert = UIAlertController(title: title, message: description, preferredStyle: .alert)
+    alert.addAction(UIAlertAction(title: "Close", style: .default, handler: { [weak self] _ in self?.dismiss(animated: true, completion: nil)}))
+    present(alert, animated: true, completion: nil)
+  }
 }
-
--- Document/ExampleProject/04-observables-in-practice/starter/Combinestagram/PhotosViewController.swift
+++ Document/ExampleProject/04-observables-in-practice/starter/Combinestagram/PhotosViewController.swift
@@ -22,114 +22,69 @@
 
 import UIKit
 import Photos
-import RxSwift
 
 class PhotosViewController: UICollectionViewController {
 
-    // MARK: public properties
-    private let selectedPhotosSubject = PublishSubject<UIImage>()
-    var selectedPhotos: Observable<UIImage> {
-        return selectedPhotosSubject.asObservable()
-    }
-    var bag = DisposeBag()
+  // MARK: public properties
 
-    // MARK: private properties
-    private lazy var photos = PhotosViewController.loadPhotos()
-    private lazy var imageManager = PHCachingImageManager()
+  // MARK: private properties
+  private lazy var photos = PhotosViewController.loadPhotos()
+  private lazy var imageManager = PHCachingImageManager()
 
-    private lazy var thumbnailSize: CGSize = {
-        let cellSize = (self.collectionViewLayout as! UICollectionViewFlowLayout).itemSize
-        return CGSize(width: cellSize.width * UIScreen.main.scale,
-                      height: cellSize.height * UIScreen.main.scale)
-    }()
+  private lazy var thumbnailSize: CGSize = {
+    let cellSize = (self.collectionViewLayout as! UICollectionViewFlowLayout).itemSize
+    return CGSize(width: cellSize.width * UIScreen.main.scale,
+                  height: cellSize.height * UIScreen.main.scale)
+  }()
 
-    static func loadPhotos() -> PHFetchResult<PHAsset> {
-        let allPhotosOptions = PHFetchOptions()
-        allPhotosOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
-        return PHAsset.fetchAssets(with: allPhotosOptions)
-    }
+  static func loadPhotos() -> PHFetchResult<PHAsset> {
+    let allPhotosOptions = PHFetchOptions()
+    allPhotosOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+    return PHAsset.fetchAssets(with: allPhotosOptions)
+  }
 
-    // MARK: View Controller
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // # Reload the photos collection when access is granted
-        let authorize = PHPhotoLibrary.authorized.share()
-        authorize
-            .skipWhile { return !$0 }
-            .take(1).subscribe(onNext: { [weak self] _ in
-                guard let this = self else { return }
-                DispatchQueue.main.async {
-                    this.photos = PhotosViewController.loadPhotos()
-                    this.collectionView?.reloadData()
-                }
-            }).disposed(by: bag)
-        // # Display an error message if the user doesnʼt grant access
-        authorize
-            .skip(1)
-            .takeLast(1)
-            .filter { return !$0 }
-            .subscribe(onNext: { [weak self] _ in
-                guard let this = self else { return }
-                DispatchQueue.main.async {
-                    this.errorMessage()
-                }
-            }
-        ).disposed(by: bag)
-    }
+  // MARK: View Controller
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        selectedPhotosSubject.onCompleted()
-    }
+  }
 
-    // MARK: UICollectionView
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
 
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return photos.count
+  }
+
+  // MARK: UICollectionView
+
+  override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return photos.count
+  }
+
+  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+    let asset = photos.object(at: indexPath.item)
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! PhotoCell
+
+    cell.representedAssetIdentifier = asset.localIdentifier
+    imageManager.requestImage(for: asset, targetSize: thumbnailSize, contentMode: .aspectFill, options: nil, resultHandler: { image, _ in
+      if cell.representedAssetIdentifier == asset.localIdentifier {
+        cell.imageView.image = image
+      }
+    })
+
+    return cell
+  }
+
+  override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    let asset = photos.object(at: indexPath.item)
+
+    if let cell = collectionView.cellForItem(at: indexPath) as? PhotoCell {
+      cell.flash()
     }
 
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-
-        let asset = photos.object(at: indexPath.item)
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! PhotoCell
-
-        cell.representedAssetIdentifier = asset.localIdentifier
-        imageManager.requestImage(for: asset, targetSize: thumbnailSize, contentMode: .aspectFill, options: nil, resultHandler: { image, _ in
-            if cell.representedAssetIdentifier == asset.localIdentifier {
-                cell.imageView.image = image
-            }
-        })
-
-        return cell
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let asset = photos.object(at: indexPath.item)
-
-        if let cell = collectionView.cellForItem(at: indexPath) as? PhotoCell {
-            cell.flash()
-        }
-
-        imageManager.requestImage(for: asset, targetSize: view.frame.size, contentMode: .aspectFill, options: nil, resultHandler: { [weak self] image, info in
-            guard let image = image, let info = info else { return }
-            if let isThumbnail = info[PHImageResultIsDegradedKey as NSString] as? Bool,
-                !isThumbnail {
-                self?.selectedPhotosSubject.onNext(image)
-            }
-        })
-    }
-
-    // # Display an error message if the user doesnʼt grant access
-    private func errorMessage() {
-        alert(title: "No access to Camera Roll",
-              message: "You can grant access to Combinestagram from the Settings app")
-            // # Completing a subscription after given time interval
-            .take(5, scheduler: MainScheduler.instance)
-            .subscribe(onCompleted: { [weak self] in
-                self?.dismiss(animated: true, completion: nil)
-                _ = self?.navigationController?.popViewController(animated: true)
-            })
-            .disposed(by: bag)
-    }
+    imageManager.requestImage(for: asset, targetSize: view.frame.size, contentMode: .aspectFill, options: nil, resultHandler: { [weak self] image, info in
+      guard let image = image, let info = info else { return }
+      
+    })
+  }
 }
-
